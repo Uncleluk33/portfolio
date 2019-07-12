01f2c24b4773d344339286d7241fd75609de891f
tsahai clahar <uncleluk33@gmail.com>
2019-07-12 15:10:08 -0500

    name change

--- server.js
+++ server.js
@@ -9,7 +9,7 @@ const app = express()
 app.use("/", serveStatic (path.join(__dirname, "/")))
 
 app.get("/", function(req, res) {
-    res.sendFile(__dirname + "/box.html")
+    res.sendFile(__dirname + "/index.html")
 })
 
 const port = process.env.PORT || 5000
